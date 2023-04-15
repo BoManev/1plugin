3bce7cc50f9d689a2bd08b15144b2afae244479a
BoManev <bomanev@pm.me>
2023-04-15 13:58:47 -0400

    treesitter queries to parse norg code blocks and send to e repl

--- lua/1plugin.lua
+++ lua/1plugin.lua
@@ -42,4 +42,43 @@ M.runsmt = function()
   })
 end
 
+M.runcode = function()
+  local query_string = '((ranged_verbatim_tag (tag_parameters ((tag_param) @tag (#match? @tag "fennel"))) content: (ranged_verbatim_tag_content) @content) @block)'
+  local parser = require('nvim-treesitter.parsers').get_parser()
+  local ok, query = pcall(vim.treesitter.query.parse, parser:lang(), query_string)
+  if not ok then
+    print('Not inside .fnl')
+    return
+  end
+  local tree = parser:parse()[1]
+  for id, node, metadata in query:iter_captures(tree:root(), 0, 0, -1) do
+    print(id, node, metadata)
+    if node and node:type() == 'ranged_verbatim_tag_content' then
+      local capture = vim.treesitter.get_node_text(node, 0)
+      for line in capture:gmatch("([^\n]*)\n?") do
+        line = line:gsub("^%s+", ""):gsub("%s+$", "")
+        if line ~= "" then
+          M.replit(line)
+        end
+      end
+    end
+  end
+
+end
+
+M.replit = function(code)
+  local eval = require("conjure.eval")
+  local client = require("conjure.client")
+  local text = require("conjure.text")
+
+  client["with-filetype"]("fennel", eval["eval-str"], {
+    origin = "my-awesome-plugin",
+    code = code, -- Use the captured body as the code to be evaluated
+    ["on-result"] = function (r)
+      local clean = text["strip-ansi-escape-sequences"](r)
+      print("RESULT:", r)
+      print("CLEANED:", clean)
+    end
+  })
+end
 return M
